dea015e8a0c43d6c3e4d08aba32daaaf9638ad71
Roberto <robertosnpiii@gmail.com>
2024-09-05 20:38:03 -0300

    v0.04

---
+++ Chapter-01/Exercicio1$2.java
@@ -0,0 +1,21 @@
+public class Exercicio1$2
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("   *\n  ***\n *****\n******* ");    
+    }    
+}
+
+/* 
+public class Exercicio1$2
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" ******");
+        System.out.println("*******");    
+    }    
+}
+
+*/
