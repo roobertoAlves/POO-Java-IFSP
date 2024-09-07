711cf30e71560cb9b29d65312fec21b95f818aa3
Roberto <robertosnpiii@gmail.com>
2024-09-06 21:46:14 -0300

    v0.05

---
+++ Chapter-01/Exercicio1$3.java
@@ -0,0 +1,19 @@
+/* 
+public class Exercicio1$3 {
+    public static void main(String[] args) {
+        System.out.println("########");
+        System.out.println("#      #");
+        System.out.println("#      #");
+        System.out.println("#      #");
+        System.out.println("#      #");
+        System.out.println("########");
+    }
+}
+*/
+public class Exercicio1$3 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("########\n#      #\n#      #\n#      #\n#      #\n########");
+    }
+}
---
+++ Chapter-01/Exercicio1$4.java
@@ -0,0 +1,21 @@
+
+public class Exercicio1$4 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("       *\n      *\n     *\n*   *\n * *\n  *");
+    }
+}
+
+// public class Exercicio1$4
+// {
+// public static void main(String[] args)
+// {
+// System.out.println(" *");
+// System.out.println(" *");
+// System.out.println(" *");
+// System.out.println("* *");
+// System.out.println(" * *");
+// System.out.println(" *");
+// }
+// }
