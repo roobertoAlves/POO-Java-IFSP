34e41d2b833b9b3a3015cd5c39181ba10b1bffd4
Roberto <robertosnpiii@gmail.com>
2024-09-15 20:05:19 -0300

    v0.12

--- Chapter-01/Exercicio1$5.java
+++ Chapter-01/Exercicio1$5.java
@@ -2,6 +2,15 @@ public class Exercicio1$5
 {
     public static void main(String[] args) 
     {
-        System.out.println("");    
+    System.out.println("*********         ***           *            *");
+    System.out.println("*       *       *     *        ***          * *");
+    System.out.println("*       *      *       *      *****        *   *");
+    System.out.println("*       *      *       *        *         *     *");
+    System.out.println("*       *      *       *        *        *       *");
+    System.out.println("*       *      *       *        *         *     *");
+    System.out.println("*       *      *       *        *          *   *");
+    System.out.println("*       *       *     *         *           * *");
+    System.out.println("*********         ***           *            *");
+
     }
 }
